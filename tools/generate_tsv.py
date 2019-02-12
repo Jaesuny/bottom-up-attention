cc8231f5c758b45f8e360e6ba27296f4b69325ff
Jaesun Park <diligensloth@gmail.com>
2019-02-12 10:01:04 +0000

    Fix bug

--- tools/generate_tsv.py
+++ tools/generate_tsv.py
@@ -72,7 +72,7 @@ def load_image_ids(split_name):
     elif split_name == 'vrd_test':
       anno_path = '/gfs/users/jayden/Multimodal/Projects/RR/data/VRD/'
       img_path = '/gfs/users/jayden/data/sg_dataset/sg_vrd_test_images/'
-      with open(anno_path + 'annotations_vrd_test.json') as f:
+      with open(anno_path + 'annotations_test.json') as f:
         for item in json.load(f):
           filepath = os.path.join(img_path, item)
           split.append((filepath,item))
